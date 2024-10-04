4bcb61ea5364958762fc5245de63eba7aff74a3f
OptimalAches <sachanakshat09@gmail.com>
2024-10-04 22:29:23 +0530

    removed error

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,22 +1,22 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-    //const mongoURI = process.env.MONGODB_URI;
-    try {
-      // const conn = await mongoose.connect(`mongodb://localhost:27017/BeyondDualism`, {
-      //   useNewUrlParser: true,
-      // });
-      // const conn = await mongoose.connect(mongoURI, {
-      //   useNewUrlParser: true,
-      // });
-      const conn = await mongoose.connect(`mongodb+srv://sachanakshat09:BqgYQ9ss8H3YsFs6@cluster0.vgo8s.mongodb.net/beyonddualism`, {
-        useNewUrlParser: true,
-      });
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-      console.error(error.message);
-      process.exit(1);
-    }
+  //const mongoURI = process.env.MONGODB_URI;
+  try {
+    // const conn = await mongoose.connect(`mongodb://localhost:27017/BeyondDualism`, {
+    //   useNewUrlParser: true,
+    // });
+    // const conn = await mongoose.connect(mongoURI, {
+    //   useNewUrlParser: true,
+    // });
+    const conn = await mongoose.connect(`mongodb+srv://sachanakshat09:BqgYQ9ss8H3YsFs6@cluster0.vgo8s.mongodb.net/beyonddualism`, {
+      useUnifiedTopology: true
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
 }
 
 export default connectDb;
