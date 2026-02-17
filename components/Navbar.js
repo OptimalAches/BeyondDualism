fe3e7a48443abedc30547a99e5d62c0e0f214e78
OptimalAches <sachanakshat09@gmail.com>
2026-02-18 03:32:20 +0530

    improved css

--- components/Navbar.js
+++ components/Navbar.js
@@ -164,7 +164,7 @@ const Navbar = () => {
                         onClick={() => { setshowdropdown(!showdropdown) }}
                         onBlur={() => { setTimeout(() => { setshowdropdown(false) }, 100) }}
                         id="dropdownDefaultButton"
-                        className="text-white bg-blue-700 hover:bg-blue-800 mx-4 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        className="text-white bg-blue-950 hover:bg-blue-900 hover:ring-2 hover:ring-purple-500 mx-4 focus:ring-4 focus:outline-none focus:ring-purple-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center transition-all duration-200 shadow-lg border border-purple-600"
                         type="button">
                         Welcome {session.user.email}
                         <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -197,7 +197,7 @@ const Navbar = () => {
 
                 {!session && (
                     <Link href={"/login"}>
-                        <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        <button type="button" className="text-white bg-blue-950 hover:bg-blue-900 hover:ring-2 hover:ring-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition-all duration-200 shadow-lg border border-purple-600">
                             Log in
                         </button>
                     </Link>
