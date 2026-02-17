057e4024124f502c92f46ef2090df0a99df14d61
OptimalAches <sachanakshat09@gmail.com>
2026-02-18 01:40:09 +0530

    improved UX

--- components/Navbar.js
+++ components/Navbar.js
@@ -187,13 +187,13 @@ const Navbar = () => {
                     </div>
                 </>}
 
-                {session && (
+                {/* {session && (
                     <button type="button"
                         className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                         onClick={() => { signOut() }}>
                         Logout
                     </button>
-                )}
+                )} */}
 
                 {!session && (
                     <Link href={"/login"}>
